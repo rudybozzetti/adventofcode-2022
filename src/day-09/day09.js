7e6ca03e1ab950a236cbf36fc241b723130c0cdc
rudy <rudy.bozzetti@gmail.com>
2022-12-11 17:07:09 +0100

    feat: day 09 part 2

--- src/day-09/day09.js
+++ src/day-09/day09.js
@@ -73,12 +73,130 @@ export const part1 = data => {
 
 
 
+
   const singleVisited = getSingleVisited(visited)
 
   return singleVisited.length
 
 }
 
-export const part2 = data => {
+export const farRight = (knot, precedingKnot) => {
+  return (precedingKnot[0] - knot[0]) > 1 && (precedingKnot[1] - knot[1]) === 0
+}
 
+export const farLeft = (knot, precedingKnot) => {
+  return (precedingKnot[0] - knot[0]) < -1 && (precedingKnot[1] - knot[1]) === 0
+}
+
+export const farUp = (knot, precedingKnot) => {
+  return (precedingKnot[1] - knot[1]) > 1 && (precedingKnot[0] - knot[0]) === 0
+}
+
+export const farDown = (knot, precedingKnot) => {
+  return ((precedingKnot[1] - knot[1])) < -1 && (precedingKnot[0] - knot[0]) === 0
+}
+
+
+export const getDistanceV = ([kx, ky], [pkx, pky]) => {
+  return [pkx - kx, pky - ky]
+}
+
+//
+export const moveKnot = (knot, precedingKnot, direction) => {
+
+  if (precedingKnot) {
+    const distance = getDistance(precedingKnot, knot)
+
+    //  console.log('### distance', distance, knot, precedingKnot)
+    const fRight = farRight(knot, precedingKnot)
+    const fLeft = farLeft(knot, precedingKnot)
+    const fUp = farUp(knot, precedingKnot)
+    const fDown = farDown(knot, precedingKnot)
+
+    const [dx, dy] = getDistanceV(knot, precedingKnot)
+
+
+    const nextPosition = fRight ? [precedingKnot[0] - 1, knot[1]] :
+      fLeft ? [precedingKnot[0] + 1, knot[1]] :
+        fUp ? [knot[0], precedingKnot[1] - 1] :
+          fDown ? [knot[0], precedingKnot[1] + 1] :
+            distance > Math.SQRT2 && dx > 0 && dy > 0 ? [knot[0] + 1, knot[1] + 1] :
+              distance > Math.SQRT2 && dx < 0 && dy > 0 ? [knot[0] - 1, knot[1] + 1] :
+                distance > Math.SQRT2 && dx > 0 && dy < 0 ? [knot[0] + 1, knot[1] - 1] :
+                  distance > Math.SQRT2 && dx < 0 && dy < 0 ? [knot[0] - 1, knot[1] - 1]
+                    : knot
+
+    return nextPosition
+  } else {
+    const nextPosition = [
+      direction === 'R' ? knot[0] + 1 : direction === 'L' ? knot[0] - 1 : knot[0],
+      direction === 'U' ? knot[1] + 1 : direction === 'D' ? knot[1] - 1 : knot[1],
+    ]
+    return nextPosition
+  }
+
+}
+
+export const moveKnots = (initialKnots, direction, amount) => {
+  return Array.from({ length: amount }).reduce(({ knots, visited }, _) => {
+    //const nextPosition = singleMove(position, direction)
+
+
+    const newPositions = knots.reduce((acc, knot, index) => {
+      //  console.log('### moveKnot knot', knot, 'prec', index === 0 ? null : acc[acc.length - 1])
+      const newKnotPos = moveKnot(knot, index === 0 ? null : acc[acc.length - 1], direction)
+      //  console.log('### moveKnot knot result', newKnotPos)
+
+
+
+      return [
+        ...acc,
+        newKnotPos
+      ]
+    }, [])
+
+    //  console.log('### newPositions after ', direction, amount, '#', newPositions)
+    //
+    const newVisited = newPositions[newPositions.length - 1].join(',')
+    //
+    return {
+      knots: newPositions,
+      visited: Array.from(new Set([...visited, newVisited]))
+    }
+  }, {
+    knots: initialKnots,
+    visited: []
+  })
+}
+
+
+export const part2 = data => {
+  const moves = buildMoves(data)
+
+  const initialKnots = Array.from({ length: 10 }).map(x => ([11, 5]))
+
+
+  const { visited } = moves.reduce((acc, [direction, amount]) => {
+    const {
+      knots,
+      visited
+    } = moveKnots(acc.knots, direction, amount)
+
+
+    return {
+      knots,
+      visited: [...acc.visited, ...visited]
+    }
+  }, {
+    knots: initialKnots,
+    visited: []
+  })
+
+
+  //  console.log('### visited', visited)
+
+  const singleVisited = new Set(visited)
+  //  console.log('### singleVisited', singleVisited)
+
+  return singleVisited.size
 }
--- src/day-09/day09.test.js
+++ src/day-09/day09.test.js
@@ -1,5 +1,5 @@
 
-import { buildMoves, getDistance, singleMove, move, part1, part2 } from './day09'
+import { buildMoves, getDistance, singleMove, move, part1, moveKnot, moveKnots, part2 } from './day09'
 
 const testInput = `R 4
 U 4
@@ -9,6 +9,15 @@ R 4
 D 1
 L 5
 R 2`
+
+const testInput2 = `R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20`
 
 const testMoves = [
   ['R', 4],
@@ -64,7 +73,68 @@ describe('day09', () => {
 
   })
 
-  it('part2', () => {
+  //
 
+  it('moveKnot', () => {
+
+    expect(moveKnot([0, 0], [0, 0], 'R')).toStrictEqual([0, 0])
+    expect(moveKnot([4, 3], [4, 4], 'L')).toStrictEqual([4, 3])
+    //
+    expect(moveKnot([1, 0], [3, 0], 'R')).toStrictEqual([2, 0])
+    expect(moveKnot([3, 0], [1, 0], 'L')).toStrictEqual([2, 0])
+    expect(moveKnot([1, 3], [1, 1], 'D')).toStrictEqual([1, 2])
+    expect(moveKnot([3, 1], [1, 1], 'U')).toStrictEqual([2, 1])
+
+
+    // diag
+    expect(moveKnot([1, 1], [2, 3], 'U')).toStrictEqual([2, 2])
+    expect(moveKnot([1, 1], [3, 2], 'R')).toStrictEqual([2, 2])
+    expect(moveKnot([3, 2], [1, 1], 'L')).toStrictEqual([2, 1])
+    expect(moveKnot([1, 2], [3, 1], 'R')).toStrictEqual([2, 1])
+
+    expect(moveKnot([3, 0], [1, 1], 'R')).toStrictEqual([2, 1])
+    expect(moveKnot([3, 0], [1, 1], 'R')).toStrictEqual([2, 1])
+
+  })
+
+  it('moveKnots', () => {
+    const step0 = Array.from({ length: 10 }).map(_ => ([11, 5]))
+    const step1 = [[16, 5], [15, 5], [14, 5], [13, 5], [12, 5], [11, 5], [11, 5], [11, 5], [11, 5], [11, 5]]
+    const step2 = [[16, 13], [16, 12], [16, 11], [16, 10], [16, 9], [15, 9], [14, 8], [13, 7], [12, 6], [11, 5]]
+    const step3 = [[8, 13], [9, 13], [10, 13], [11, 13], [12, 13], [12, 12], [12, 11], [12, 10], [12, 9], [12, 8]]
+    const step4 = [[8, 10], [8, 11], [9, 12], [10, 12], [11, 12], [12, 12], [12, 11], [12, 10], [12, 9], [12, 8]]
+
+
+
+    expect(moveKnots(step0, 'R', 5)).toStrictEqual({
+      knots: step1,
+      visited: ['11,5']
+    })
+
+    expect(moveKnots(step1, 'U', 8)).toStrictEqual({
+      knots: step2,
+      visited: ['11,5']
+    })
+
+    expect(moveKnots(step2, 'L', 8)).toStrictEqual({
+      knots: step3,
+      visited: ['11,5', '12,6', '13,7', '12,8']
+    })
+
+    expect(moveKnots(step3, 'D', 3)).toStrictEqual({
+      knots: step4,
+      visited: ['12,8']
+    })
+
+
+
+    //const { knots } = moveKnots(step1, 'U', 8)
+    //console.log('### knots', knots)
+
+
+  })
+
+  it('part2', () => {
+    expect(part2(testInput2)).toBe(36)
   })
 })
