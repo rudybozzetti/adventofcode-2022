7e2da848804a6a3a3ceb245a1126973a59aa8aaa
rudy <rudy.bozzetti@gmail.com>
2022-12-21 08:58:07 +0100

    wip

--- src/day-17/day17.js
+++ src/day-17/day17.js
@@ -37,7 +37,7 @@ const SHAPES = [
 
 const SCREEN_WIDTH = 7
 
-export const updateScreen = (prevScreen = [], shapeIndex, shapePosition) => {
+export const prepareScreen = (prevScreen = [], shapeIndex, shapePosition) => {
   const shape = SHAPES[shapeIndex]
 
   const shapeHeight = shape.length
@@ -45,10 +45,20 @@ export const updateScreen = (prevScreen = [], shapeIndex, shapePosition) => {
   //
   const newLines = [...Array(h)].map(_ => Array(SCREEN_WIDTH).fill('.'))
 
-  const tmp = [
+  return [
     ...prevScreen,
     ...newLines
   ]
+
+}
+
+export const updateScreen = (prevScreen = [], shapeIndex, shapePosition) => {
+  const shape = SHAPES[shapeIndex]
+
+  const shapeHeight = shape.length
+  const h = shapePosition.y + shapeHeight
+  //
+
 
   for (let i = 0; i < shape.length; i++) {
     /*
@@ -57,8 +67,8 @@ export const updateScreen = (prevScreen = [], shapeIndex, shapePosition) => {
     }
     */
     for (let j = 0; j < SCREEN_WIDTH; j++) {
-      console.log('### i',i, 'j',j,'shapePosition.x',shapePosition.x, '+ shape.0.len',shapePosition.x + shape[0].length  )
-      tmp[shapePosition.y + i][j] = shapePosition.x <= j && j < shapePosition.x + shape[0].length ? shape[i][j - shapePosition.x ] : '.'
+      console.log('### i', i, 'j', j, 'shapePosition.x', shapePosition.x, '+ shape.0.len', shapePosition.x + shape[0].length)
+      tmp[shapePosition.y + i][j] = shapePosition.x <= j && j < shapePosition.x + shape[0].length ? shape[i][j - shapePosition.x] : '.'
 
 
     }
@@ -79,22 +89,58 @@ export const debugScreen = (screen) => {
 }
 
 export const tryMoveLeft = (screen, shapeIndex, shapePosition) => {
+  const shape = SHAPES[shapeIndex]
+
   if (shapePosition.x === 0) {
     return false
   }
 
-  const shape = SHAPES[shapeIndex]
-
   for (let i = 0; i < shape.length; i++) {
-    console.log('### shapePosition.y+i', shapePosition.y + i, '### shapePosition.x-1', shapePosition.x - 1)
-    if (screen[shapePosition.y + i][shapePosition.x - 1] === '#' &&
-      shape[shapePosition.y + i][shapePosition.x] === '#') {
-      return false
+    //  console.log('### shapePosition.y+i', shapePosition.y + i, '### shapePosition.x-1', shapePosition.x - 1)
+    for (let j = 0; j < shape[0].length; j++) {
+      if (screen[shapePosition.y + i][shapePosition.x + j - 1] === '#' &&
+        shape[i][j] === '#') {
+        return false
+      }
     }
   }
 
   return true
+}
 
+export const tryMoveRight = (screen, shapeIndex, shapePosition) => {
+  const shape = SHAPES[shapeIndex]
+
+  if (shapePosition.x + shape[0].length > 6) {
+    return false
+  }
+
+  for (let i = 0; i < shape.length; i++) {
+    //  console.log('### shapePosition.y+i', shapePosition.y + i, '### shapePosition.x-1', shapePosition.x - 1)
+    for (let j = 0; j < shape[0].length; j++) {
+      if (screen[shapePosition.y + i][shapePosition.x + j - 1] === '#' &&
+        shape[i][j] === '#') {
+        return false
+      }
+    }
+  }
+
+  return true
+}
+
+export const tryMoveDown = (screen, shapeIndex, shapePosition) => {
+  const shape = SHAPES[shapeIndex]
+
+  if (shapePosition.y === 0) {
+    return false
+  }
+
+  for (let j = 0; j < shape[0].length; j++) {
+    if (screen[shapePosition.y - 1][shapePosition.x] === '#' &&
+      shape[0][j] === '#') {
+      return false
+    }
+  }
 }
 
 export const play = (counterLimit = 1, moves) => {
@@ -111,10 +157,9 @@ export const play = (counterLimit = 1, moves) => {
     //
     if (settled) {
       shapeCoords = { x: 2, y: ymax + 3 }
-      screen = updateScreen(screen, shapeIndex, shapeCoords)
+      screen = prepareScreen(screen, shapeIndex, shapeCoords)
       settled = false
-      //  questo quando non può più fare move down
-      //  shapeIndex = (shapeIndex + 1) % SHAPES.length
+
     }
 
     //  move
@@ -123,18 +168,25 @@ export const play = (counterLimit = 1, moves) => {
 
     if (move === '<' && tryMoveLeft(screen, shapeIndex, shapeCoords)) {
       shapeCoords.x = shapeCoords.x - 1
-      screen = updateScreen(screen, shapeIndex, shapeCoords)
     }
 
+    if (move === '>' && tryMoveRight(screen, shapeIndex, shapeCoords)) {
+      shapeCoords.x = shapeCoords.x + 1
+    }
 
+    //  down
+    if (tryMoveDown) {
+      shapeCoords.y = shapeCoords.y - 1
+    } else {
+      screen = updateScreen(screen, shapeIndex, shapeCoords)
+      settled = true
+      const shape = SHAPES[shapeIndex]
+      ymax = shapeCoords.y + shape.length - 1
+      shapeIndex = (shapeIndex + 1) % SHAPES.length
+      //
+      counter++
+    }
 
-
-
-
-
-
-    //
-    counter++
   }
 
   return screen
