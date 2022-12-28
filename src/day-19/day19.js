b30f9000aaf9ab7cd2b78c5b20505a6b77fa5347
rudy <rudy.bozzetti@gmail.com>
2022-12-28 01:05:38 +0100

    wip: day 19

--- src/day-19/day19.js
+++ src/day-19/day19.js
@@ -3,8 +3,8 @@ export const buildModel = input => {
   return input.split(/\n/).reduce((acc, line) => {
     const [
       [id],
-      [oreRobotCost],
-      [clayRobotCost],
+      [oreRobotOreCost],
+      [clayRobotOreCost],
       [obsidianRobotOreCost],
       [obsidianRobotClayCost],
       [geodeRobotOreCost],
@@ -15,8 +15,8 @@ export const buildModel = input => {
       ...acc,
       {
         id: parseInt(id),
-        oreRobotCost: parseInt(oreRobotCost),
-        clayRobotCost: parseInt(clayRobotCost),
+        oreRobotOreCost: parseInt(oreRobotOreCost),
+        clayRobotOreCost: parseInt(clayRobotOreCost),
         obsidianRobotOreCost: parseInt(obsidianRobotOreCost),
         obsidianRobotClayCost: parseInt(obsidianRobotClayCost),
         geodeRobotOreCost: parseInt(geodeRobotOreCost),
@@ -25,4 +25,113 @@ export const buildModel = input => {
     ]
 
   }, [])
+}
+
+const ops = ['nop', 'oreBot', 'clayBot', 'obsidianBot', 'geodeBot']
+
+export const initialState = {
+  ore: 0,
+  clay: 0,
+  obsidian: 0,
+  geode: 0,
+  time: 24,
+  bots: {
+    ore: 1,
+    clay: 0,
+    obsidian: 0,
+    geode: 0
+  },
+  choices: []
+}
+
+export const getSolutionVote = (blueprint, solution) => {
+  const oreBotFactor = blueprint.oreRobotOreCost
+  const clayBotFactor = blueprint.clayRobotOreCost
+  const obsidianBotFactor = blueprint.obsidianRobotOreCost + blueprint.obsidianRobotClayCost * blueprint.clayRobotOreCost
+  const geodeBotFactor = blueprint.geodeRobotOreCost + blueprint.geodeRobotObsidianCost * (blueprint.obsidianRobotOreCost + blueprint.obsidianRobotClayCost * blueprint.clayRobotOreCost)
+  //
+  return solution.bots.ore * oreBotFactor +
+    solution.bots.clay * clayBotFactor +
+    solution.bots.obsidian * obsidianBotFactor +
+    solution.bots.geode * geodeBotFactor
+
+}
+
+export const getBlueprintSolutions = (blueprint, state, op) => {
+
+  const tmpState = {
+    ...state,
+    choices: [...state.choices, op],
+    //  update mats
+    ore: state.ore + state.bots.ore - (
+      op === 'oreBot' ? blueprint.oreRobotOreCost :
+        op === 'clayBot' ? blueprint.clayRobotOreCost :
+          op === 'obsidianBot' ? blueprint.obsidianRobotOreCost :
+            op === 'geodeBot' ? blueprint.geodeRobotOreCost :
+              0),
+    clay: state.clay + state.bots.clay - (
+      op === 'obsidianBot' ? blueprint.obsidianRobotClayCost : 0),
+    obsidian: state.obsidian + state.bots.obsidian - (
+      op === 'geodeBot' ? blueprint.geodeRobotObsidianCost : 0),
+    geode: state.geode + state.bots.geode,
+    //  update time
+    time: state.time - 1,
+    //  update bots
+    bots: {
+      ore: state.bots.ore + (op === 'oreBot' ? 1 : 0),
+      clay: state.bots.clay + (op === 'clayBot' ? 1 : 0),
+      obsidian: state.bots.obsidian + (op === 'obsidianBot' ? 1 : 0),
+      geode: state.bots.geode + (op === 'geodeBot' ? 1 : 0),
+    }
+  }
+
+  const solutions = [{ ...tmpState, vote: getSolutionVote(blueprint, tmpState) }]
+
+  if (tmpState.time <= 4) {
+    return solutions
+  }
+
+  const nextAvailOps = [
+    'nop',
+    tmpState.ore >= blueprint.oreRobotOreCost ? 'oreBot' : undefined,
+    tmpState.ore >= blueprint.clayRobotOreCost ? 'clayBot' : undefined,
+    tmpState.ore >= blueprint.obsidianRobotOreCost && tmpState.clay >= blueprint.obsidianRobotClayCost ? 'obsidianBot' : undefined,
+    tmpState.ore >= blueprint.geodeRobotOreCost && tmpState.obsidian >= blueprint.geodeRobotObsidianCost ? 'geodeBot' : undefined
+  ].filter(o => !!o)
+
+  //  console.log('### time', tmpState.time, 'state', tmpState, 'next avail ops', nextAvailOps)
+
+  let solutionVove = 0
+  for (const nextOp of nextAvailOps) {
+    const nextSolutions = getBlueprintSolutions(blueprint, tmpState, nextOp).map(s => ({
+      ...s,
+      vote: getSolutionVote(blueprint, s)
+    })).sort((a, b) => b.vote - a.vote)
+
+    for (const s of nextSolutions) {
+      if (nextSolutions.find((o) => o !== s && o.vote > s.vote)) {
+        //  se trovo una soluzione migliore, evito di accodare
+        continue
+      }
+
+      solutions.push(s)
+    }
+
+  }
+
+  return solutions
+
+}
+
+export const part1 = inputData => {
+  const blueprints = buildModel(inputData)
+
+  const solutions = getBlueprintSolutions(blueprints[0], initialState, 'nop')
+  const sorted = solutions.sort((a, b) => a.time === b.time ? b.geode - a.geode : a.time - b.time)
+  console.log("\n\n### solutions", sorted.slice(0, 10))
+
+}
+
+export const part2 = inputData => {
+
 }
--- src/day-19/day19.test.js
+++ src/day-19/day19.test.js
@@ -1,12 +1,43 @@
-import { buildModel } from './day19'
+import { buildModel, getBlueprintSolutions, initialState } from './day19'
 
 const testInput = `Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
 Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.`
 
+
+const testBuileprint1 = {
+  id: 1,
+  oreRobotOreCost: 4,
+  clayRobotOreCost: 2,
+  obsidianRobotOreCost: 3,
+  obsidianRobotClayCost: 14,
+  geodeRobotOreCost: 2,
+  geodeRobotObsidianCost: 7
+}
+
+const testBuileprint2 = {
+  id: 2,
+  oreRobotOreCost: 2,
+  clayRobotOreCost: 3,
+  obsidianRobotOreCost: 3,
+  obsidianRobotClayCost: 8,
+  geodeRobotOreCost: 3,
+  geodeRobotObsidianCost: 12
+}
+
+
 describe('day19', () => {
   it('buildModel', () => {
-
-    buildModel(testInput)
-
+    expect(buildModel(testInput)).toStrictEqual([testBuileprint1, testBuileprint2])
   })
+
+  it('getBlueprintSolutions', () => {
+    const solutions = getBlueprintSolutions(testBuileprint1, initialState, 'nop')
+    //
+    //  console.log('### solutions', solutions)
+    const sorted = solutions.sort((a, b) => b.geode - a.geode)
+    //
+    console.log('### sorted', sorted.slice(0, 3))
+  })
+
+
 })
