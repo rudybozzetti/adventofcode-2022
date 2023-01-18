5c049365ee97747763cde653037c25a2880af680
rudy <rudy.bozzetti@gmail.com>
2023-01-18 01:03:22 +0100

    wip: day 24

--- src/day-24/day24.js
+++ src/day-24/day24.js
@@ -44,3 +44,173 @@ export const parseInput = input => {
     }
   }, { valley: {}, blizzards: [] })
 }
+
+export const updateBlizzards = ({ minx, miny, maxx, maxy }, blizzards) => {
+  return blizzards.map(({ dir, x, y }) => {
+    switch (dir) {
+      case '>':
+        return x + 1 === maxx ? {
+          dir,
+          x: minx + 1,
+          y
+        } : {
+          dir,
+          x: x + 1,
+          y
+        }
+
+      case '<':
+        return x - 1 === minx ? {
+          dir,
+          x: maxx - 1,
+          y
+        } : {
+          dir,
+          x: x - 1,
+          y
+        }
+
+      case '^':
+        return y - 1 === miny ? {
+          dir,
+          x,
+          y: maxy - 1
+        } : {
+          dir,
+          x,
+          y: y - 1
+        }
+
+      case 'v':
+        return y + 1 === maxy ? {
+          dir,
+          x,
+          y: miny + 1
+        } : {
+          dir,
+          x,
+          y: y + 1
+        }
+
+    }
+
+  })
+}
+
+const getVisitedKey = (x, y, blizzards) => {
+  return JSON.stringify({ x, y, blizzards })
+}
+
+export const solve = ({ start, end, ...bounds }, initialBlizzards) => {
+  const stack = [{
+    x: start.x,
+    y: start.y,
+    blizzards: initialBlizzards,
+    moves: 0,
+    steps: []
+  }]
+
+  let bestMoves = Number.POSITIVE_INFINITY
+
+  const visited = new Map()
+
+  while (stack.length > 0) {
+    const { x, y, blizzards, moves, steps } = stack.pop()
+    console.log('### stack pop x, y', x, y, '### moves', moves, '### steps', steps)
+
+    if (moves > 100000) {
+      break
+    }
+
+    //
+    if (moves > bestMoves) {
+      console.log('### moves > bestMoves', moves, '>', bestMoves, '>>> exit')
+      continue
+    }
+
+    const newSteps = [...steps, { x, y }]
+
+    //
+    const nextBlizzards = updateBlizzards(bounds, blizzards)
+    //  console.log('### nextBlizzards', nextBlizzards)
+    //
+    const visitedKey = getVisitedKey(x, y, nextBlizzards)
+    if (visited.has(visitedKey)) {
+      console.log('### already visited', visitedKey)
+      continue
+    }
+    visited.set(visitedKey)
+
+    //
+    if (x === end.x && y === end.y) {
+      console.log('### found exit in moves', moves)
+      if (moves < bestMoves) {
+        console.log('### new best moves', moves)
+        bestMoves = moves
+      }
+      continue
+    }
+
+    //  try next is exit
+    if (x === end.x && y + 1 === end.y) {
+      console.log('### found exit')
+      stack.push({
+        x, y: y + 1, blizzards: nextBlizzards, moves: moves + 1, steps: newSteps
+      })
+      continue
+    }
+
+    //  nop action
+    console.log('### try nop')
+    stack.push({
+      x, y, blizzards: nextBlizzards, moves: moves + 1, steps: newSteps
+    })
+
+    //  try '>'
+    if (y > bounds.miny && x < (bounds.maxx - 1) && !nextBlizzards.some(b => b.x === x + 1 && b.y === y)) {
+      console.log('### try >')
+      stack.push({
+        x: x + 1, y, blizzards: nextBlizzards, moves: moves + 1, steps: newSteps
+      })
+    }
+
+    //  try '<'
+    if (y > bounds.miny && x > (bounds.minx + 1) && !nextBlizzards.some(b => b.x === x - 1 && b.y === y)) {
+      console.log('### try <')
+      stack.push({
+        x: x - 1, y, blizzards: nextBlizzards, moves: moves + 1, steps: newSteps
+      })
+    }
+
+    //  try '^'
+    if (y > (bounds.miny + 1) && !nextBlizzards.some(b => b.x === x && b.y === y - 1)) {
+      console.log('### try ^')
+      stack.push({
+        x, y: y - 1, blizzards: nextBlizzards, moves: moves + 1, steps: newSteps
+      })
+    }
+
+    //  try 'v'
+    if (y < (bounds.maxy - 1) && !nextBlizzards.some(b => b.x === x && b.y === y + 1)) {
+      console.log('### try v')
+      stack.push({
+        x, y: y + 1, blizzards: nextBlizzards, moves: moves + 1, steps: newSteps
+      })
+    }
+
+  }
+
+  return bestMoves
+}
+
+export const part1 = input => {
+  const { valley, blizzards } = parseInput(input)
+
+  const result = solve(valley, blizzards)
+
+  return result
+}
+
+export const part2 = input => {
+  const { valley, blizzards } = parseInput(input)
+}
--- src/day-24/day24.test.js
+++ src/day-24/day24.test.js
@@ -1,7 +1,7 @@
 
-import {parseInput} from './day24'
+import { parseInput, updateBlizzards, solve } from './day24'
 
-const testInput=`#.######
+const testInput = `#.######
 #>>.<^<#
 #.<..<<#
 #>v.><>#
@@ -10,38 +10,64 @@ const testInput=`#.######
 
 
 const testValleyModel = {
-  minx:0,
-  miny:0,
+  minx: 0,
+  miny: 0,
   maxx: 7,
   maxy: 5,
-  start: {x:1,y:0},
-  end: {x:6,y:5}
+  start: { x: 1, y: 0 },
+  end: { x: 6, y: 5 }
 }
 
 const testBlizzardsModel = [
-  {dir: '>', x: 1,y:1},
-  {dir: '>', x: 2,y:1},
-  {dir: '<', x: 4,y:1},
-  {dir: '^', x: 5,y:1},
-  {dir: '<', x: 6,y:1},
+  { dir: '>', x: 1, y: 1 },
+  { dir: '>', x: 2, y: 1 },
+  { dir: '<', x: 4, y: 1 },
+  { dir: '^', x: 5, y: 1 },
+  { dir: '<', x: 6, y: 1 },
   //
-  {dir: '<', x: 2,y:2},
-  {dir: '<', x: 5,y:2},
-  {dir: '<', x: 6,y:2},
+  { dir: '<', x: 2, y: 2 },
+  { dir: '<', x: 5, y: 2 },
+  { dir: '<', x: 6, y: 2 },
   //
-  {dir: '>', x: 1,y:3},
-  {dir: 'v', x: 2,y:3},
-  {dir: '>', x: 4,y:3},
-  {dir: '<', x: 5,y:3},
-  {dir: '>', x: 6,y:3},
+  { dir: '>', x: 1, y: 3 },
+  { dir: 'v', x: 2, y: 3 },
+  { dir: '>', x: 4, y: 3 },
+  { dir: '<', x: 5, y: 3 },
+  { dir: '>', x: 6, y: 3 },
   //
-  {dir: '<', x: 1,y:4},
-  {dir: '^', x: 2,y:4},
-  {dir: 'v', x: 3,y:4},
-  {dir: '^', x: 4,y:4},
-  {dir: '^', x: 5,y:4},
-  {dir: '>', x: 6,y:4},
-  
+  { dir: '<', x: 1, y: 4 },
+  { dir: '^', x: 2, y: 4 },
+  { dir: 'v', x: 3, y: 4 },
+  { dir: '^', x: 4, y: 4 },
+  { dir: '^', x: 5, y: 4 },
+  { dir: '>', x: 6, y: 4 },
+
+]
+
+const testBlizzardsModelMinute1 = [
+  { dir: '>', x: 2, y: 1 },
+  { dir: '>', x: 3, y: 1 },
+  { dir: '<', x: 3, y: 1 },
+  { dir: '^', x: 5, y: 4 },
+  { dir: '<', x: 5, y: 1 },
+  //
+  { dir: '<', x: 1, y: 2 },
+  { dir: '<', x: 4, y: 2 },
+  { dir: '<', x: 5, y: 2 },
+  //
+  { dir: '>', x: 2, y: 3 },
+  { dir: 'v', x: 2, y: 4 },
+  { dir: '>', x: 5, y: 3 },
+  { dir: '<', x: 4, y: 3 },
+  { dir: '>', x: 1, y: 3 },
+  //
+  { dir: '<', x: 6, y: 4 },
+  { dir: '^', x: 2, y: 3 },
+  { dir: 'v', x: 3, y: 1 },
+  { dir: '^', x: 4, y: 3 },
+  { dir: '^', x: 5, y: 3 },
+  { dir: '>', x: 1, y: 4 },
+
 ]
 
 describe('day24', () => {
@@ -50,5 +76,16 @@ describe('day24', () => {
       valley: testValleyModel,
       blizzards: testBlizzardsModel
     })
+  })
+
+  it('updateBlizzards', () => {
+    const bounds = {
+      minx: 0, miny: 0, maxx: 7, maxy: 5
+    }
+    expect(updateBlizzards(bounds, testBlizzardsModel)).toStrictEqual(testBlizzardsModelMinute1)
+  })
+
+  it('solve', () => {
+    expect(solve(testValleyModel, testBlizzardsModel)).toBe(18)
   })
 })
